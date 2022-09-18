e8c3fbdf73ed106476344b59310e75132e68067c
Nagy Balázs <bnagy@tmit.bme.hu>
2022-09-18 14:53:57 +0200

    up

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,11 @@
 #include <BLEServer.h>
 #include <PID_v1.h>
 #include "temp_measurment.h"
+#include "globals.h"
+#include "relay_control.h"
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define RELAY_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define ADC_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a9"
 
-#define SSR_RELAY_PIN 23
 
-#define TMP_ADC_PIN 13
-
-#define N_MEAS (100)
 
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
@@ -21,8 +16,6 @@ double Setpoint, Input, Output;
 //Specify the links and initial tuning parameters
 double Kp=2, Ki=5, Kd=1;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-unsigned long windowStartTime;
-int WindowSize = 5000;
 class BLERelayCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
@@ -74,19 +67,17 @@ void setupBLE(){
 void setup() {
   //initialize the variables we're linked to
 
-  
+  MUTEX_PID_OUT_C1 = xSemaphoreCreateMutex();
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode (LED_BUILTIN, OUTPUT);
   pinMode (SSR_RELAY_PIN, OUTPUT);
   init_temp(TMP_ADC_PIN);
-  windowStartTime = millis();
   setupBLE();
-  //analogSetAttenuation(TMP_ADC_PIN, 6db);
   Input = (double) ADC_LUT[(uint32_t) analogRead(TMP_ADC_PIN)];
   Setpoint = 100;
   //turn the PID on
-  myPID.SetOutputLimits(0, WindowSize);
+  myPID.SetOutputLimits(0, 100);
   myPID.SetMode(AUTOMATIC);
 }
 
@@ -96,17 +87,18 @@ const double R=100.0;
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println("Csumi!");
-  
+  xTaskCreate(
+    RelayControl,    // Function that should be called
+    "RelayControl_C1",   // Name of the task (for debugging)
+    1000,            // Stack size (bytes)
+    NULL,            // Parameter to pass
+    1,               // Task priority
+    NULL             // Task handle
+  );
 
   Input = measure_temp(TMP_ADC_PIN);
   myPID.Compute();
-  unsigned long now = millis();
-  if (now - windowStartTime > WindowSize)
-  { //time to shift the Relay Window
-    windowStartTime += WindowSize;
-  }
-  if (Output > now - windowStartTime) digitalWrite(SSR_RELAY_PIN, HIGH);
-  else digitalWrite(SSR_RELAY_PIN, LOW);
+
   //char tmp_str[10];
   //String(tmp).toCharArray(tmp_str,10);
   //pADCCharacteristic->setValue(tmp_str);
